f4b02b131187eecda9468ea8386c655ba5c27a64
Donovan Tengblad <purplefish32@gmail.com>
2015-03-18 11:38:10 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -5,25 +5,25 @@ printf "{"
 
 # Disk
 printf "\"disk\":{";
-echo "\""
-/bin/df --total | /usr/bin/awk  '/total/ { printf "total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\""}' | /usr/bin/tr -d '\n'
+printf "\""
+echo /bin/df --total | /usr/bin/awk  '/total/ { printf "total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\""}' | /usr/bin/tr -d '\n'
 printf "},"
 
 # CPU
 printf "\"cpu\":{";
 # Count
 printf "\"count\":\""
-/bin/grep -c ^processor /proc/cpuinfo | /usr/bin/tr -d '\n'
+echo /bin/grep -c ^processor /proc/cpuinfo | /usr/bin/tr -d '\n'
 printf "\","
 
 # Load
 printf "\"load\":{\""
-/bin/cat /proc/loadavg | /usr/bin/awk '{print "min1\":\""$1"\",\"min5\":\""$2"\",\"min15\":\""$3""}' | /usr/bin/tr -d '\n'
+echo /bin/cat /proc/loadavg | /usr/bin/awk '{print "min1\":\""$1"\",\"min5\":\""$2"\",\"min15\":\""$3""}' | /usr/bin/tr -d '\n'
 printf "\"}},"
 
 # Memory
 printf "\"memory\":{";
-/usr/bin/free | /usr/bin/awk 'NR == 2 {printf "\"total\":\""$2"\",\"used\":""\""$3"\",\"free\":\""$4"\","} NR == 3 {printf "\"buffersCache\":{\"used\":\""$3"\",\"free\":\""$4"\"},"} NR ==4 {printf "\"swap\":{\"total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\"}"}'
+echo /usr/bin/free | /usr/bin/awk 'NR == 2 {printf "\"total\":\""$2"\",\"used\":""\""$3"\",\"free\":\""$4"\","} NR == 3 {printf "\"buffersCache\":{\"used\":\""$3"\",\"free\":\""$4"\"},"} NR ==4 {printf "\"swap\":{\"total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\"}"}'
 printf "}"
 
 # Json end
