c4f39993e55ab36eb7426fb28202e9e95de106fb
Donovan Tengblad <purplefish32@gmail.com>
2015-03-18 11:38:58 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -13,17 +13,17 @@ printf "},"
 printf "\"cpu\":{";
 # Count
 printf "\"count\":\""
-echo /bin/grep -c ^processor /proc/cpuinfo | /usr/bin/tr -d '\n'
+/bin/grep -c ^processor /proc/cpuinfo | /usr/bin/tr -d '\n'
 printf "\","
 
 # Load
 printf "\"load\":{\""
-echo /bin/cat /proc/loadavg | /usr/bin/awk '{print "min1\":\""$1"\",\"min5\":\""$2"\",\"min15\":\""$3""}' | /usr/bin/tr -d '\n'
+/bin/cat /proc/loadavg | /usr/bin/awk '{print "min1\":\""$1"\",\"min5\":\""$2"\",\"min15\":\""$3""}' | /usr/bin/tr -d '\n'
 printf "\"}},"
 
 # Memory
 printf "\"memory\":{";
-echo /usr/bin/free | /usr/bin/awk 'NR == 2 {printf "\"total\":\""$2"\",\"used\":""\""$3"\",\"free\":\""$4"\","} NR == 3 {printf "\"buffersCache\":{\"used\":\""$3"\",\"free\":\""$4"\"},"} NR ==4 {printf "\"swap\":{\"total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\"}"}'
+/usr/bin/free | /usr/bin/awk 'NR == 2 {printf "\"total\":\""$2"\",\"used\":""\""$3"\",\"free\":\""$4"\","} NR == 3 {printf "\"buffersCache\":{\"used\":\""$3"\",\"free\":\""$4"\"},"} NR ==4 {printf "\"swap\":{\"total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\"}"}'
 printf "}"
 
 # Json end
