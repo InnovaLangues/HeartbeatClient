f024525fc23fa9db953fd8af4fd461742495c9f5
Donovan Tengblad <purplefish32@gmail.com>
2015-05-18 16:52:50 +0200

    Added prosses

--- client.sh
+++ client.sh
@@ -30,7 +30,12 @@ printf "\"}},"
 # Memory
 printf "\"memory\":{";
 /usr/bin/free | /usr/bin/awk 'NR == 2 {printf "\"total\":\""$2"\",\"used\":""\""$3"\",\"free\":\""$4"\","} NR == 3 {printf "\"buffersCache\":{\"used\":\""$3"\",\"free\":\""$4"\"},"} NR ==4 {printf "\"swap\":{\"total\":\""$2"\",\"used\":\""$3"\",\"free\":\""$4"\"}"}'
-printf "}"
+printf "},"
+
+#Processes
+printf "\"processes\":[";
+echo $(/bin/ps axo user,comm,pcpu,vsz --sort -pcpu | /usr/bin/awk 'BEGIN{OFS=":"} NR>1 {print "{ \"user\" : \"" $1 "\", \"command\" : \"" $2 "\", \"percent_cpu\" : "$3", \"memory_used\" : "$4" },"}') | /bin/sed '$s/,$//';
+printf "]";
 
 # Json end
-printf "}"
+printf "}";
